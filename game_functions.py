aad35b760263d041de0a79701436861e6cb4b48d
hero <hero@163.com>
2020-02-19 15:26:00 +0800

    update collisions in game_functions,update_bullets()

--- game_functions.py
+++ game_functions.py
@@ -58,11 +58,14 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
 	aliens.draw(screen)
 	pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens,bullets):
 	bullets.update()
 	for bullet in bullets.copy():
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
+	collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
+	
+
 
 	
 def fire_bullet(ai_settings,screen,ship,bullets):
