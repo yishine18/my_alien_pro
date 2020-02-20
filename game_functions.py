28891753e8599c1a726ac1fe799c28f1b295f026
hero <hero@163.com>
2020-02-20 16:22:00 +0800

    update_screen check game_active

--- game_functions.py
+++ game_functions.py
@@ -49,7 +49,7 @@ def check_events(ai_settings,screen,ship,bullets):
 
 
 
-def update_screen(ai_settings,screen,ship,aliens,bullets):
+def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
 	screen.fill(ai_settings.bg_color)
 	for bullet in bullets.sprites():
 		bullet.draw_bullet()
@@ -57,6 +57,12 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
 	ship.blitme()
 	# alien.blitme()
 	aliens.draw(screen)
+	# pygame.display.flip()
+
+
+	if not stats.game_active:
+		play_button.draw_button()
+
 	pygame.display.flip()
 
 def update_bullets(ai_settings,screen,ship,aliens,bullets):
