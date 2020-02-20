1230ad372855542136dcb839f56b1afce398e222
hero <hero@163.com>
2020-02-20 17:08:14 +0800

    add increase_speed,dynamic settings,and speedup_scale

--- settings.py
+++ settings.py
@@ -19,6 +19,26 @@ class Settings():
 		#fleet_direction = -1  表示左移
 
 		self.ship_limit = 2
+
+
+		self.speedup_scale = 1.1
+		self.initialize_dynamic_settings()
+
+		def initialize_dynamic_settings(self):
+			
+			self.ship_speed_factor = 1.5
+			self.bullet_speed_factor = 3
+			self.alien_speed_factor = 1
+
+			self.fleet_direction = 1
+
+		def increase_speed(self):
+			
+			self.ship_speed_factor *= self.speedup_scale
+			self.bullet_speed_factor *= self.speedup_scale
+			self.alien_speed_factor *= self.speedup_scale
+
+			
 		
 
 		
