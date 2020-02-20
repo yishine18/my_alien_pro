78e56a547a2b164364c808c6bf461823a06a3fa3
hero <hero@163.com>
2020-02-20 14:20:11 +0800

    alien_invasion add stats,and update_aliens add pramers

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
+
 # from alien import Alien
 
 
@@ -17,6 +19,8 @@ def run_game():
 	# screen = pygame.display.set_mode((1200,800))
 	screen = pygame.display.set_mode( (ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+
+	stats = GameStats(ai_settings)
 
 	ship = Ship(ai_settings,screen)
 	bullets = Group()
@@ -32,7 +36,7 @@ def run_game():
 		gf.check_events(ai_settings,screen,ship,bullets)
 		ship.update()
 		gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-		gf.update_aliens(ai_settings,ship,aliens)
+		gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
 		# bullets.update()
 
 		# for bullet in bullets.copy():
