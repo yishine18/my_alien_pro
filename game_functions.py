35030d5c579579e38e5b326e6dc86d9d403c869a
hero <hero@163.com>
2020-02-20 16:49:35 +0800

    add reste stats in check play button,add something in check event,include paramers

--- game_functions.py
+++ game_functions.py
@@ -27,7 +27,7 @@ def check_keyup_events(event,ship):
 	elif event.key == pygame.K_LEFT:
 		ship.moving_left =False
 
-def check_events(ai_settings,screen,stats,play_button,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets):
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -47,7 +47,7 @@ def check_events(ai_settings,screen,stats,play_button,ship,bullets):
 			# 	ship.moving_left =False
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			mouse_x,mouse_y = pygame.mouse.get_pos()
-			check_play_button(stats,play_button,mouse_x,mouse_y)
+			check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,mouse_x,mouse_y)
 		
 
 
@@ -176,10 +176,18 @@ def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
 			ship_hit(ai_settings,stats,screen,ship,aliens,bullets)
 			break
 
-def check_play_button(stats,play_button,mouse_x,mouse_y):
+def check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,mouse_x,mouse_y):
 	
 	if play_button.rect.collidepoint(mouse_x,mouse_y):
+		stats.reset_stats()
 		stats.game_active = True
+
+		aliens.empty()
+		bullets.empty()
+
+		create_fleet(ai_settings,screen,ship,aliens)
+		ship.center_ship()
+
 
 
 
