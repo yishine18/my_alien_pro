2670f01632214c84f7901c27c4ba29286df8def7
hero <hero@163.com>
2020-02-20 16:03:44 +0800

    change game_active to False,when press button play,start

--- game_stats.py
+++ game_stats.py
@@ -2,8 +2,9 @@ class GameStats():
 	def __init__(self,ai_settings):
 		
 		self.ai_settings = ai_settings
-		self.game_active = True
+		# self.game_active = True
 		self.reset_stats()
+		self.game_active = False
 
 	def reset_stats(self):
 		self.ships_left = self.ai_settings.ship_limit
