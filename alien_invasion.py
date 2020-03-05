16c4f8d94077b220f74289354f1450ab063fa9aa
hero <hero@hero.com>
2020-03-05 17:04:53 +0800

    add sb to show mark on screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,60 +8,60 @@ import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
+
 
 # from alien import Alien
 
 
 def run_game():
+    pygame.init()
 
-	pygame.init()
+    ai_settings = Settings()
+    # screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
 
-	ai_settings = Settings()
-	# screen = pygame.display.set_mode((1200,800))
-	screen = pygame.display.set_mode( (ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
 
-	play_button = Button(ai_settings,screen,"Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
 
-	stats = GameStats(ai_settings)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
-	ship = Ship(ai_settings,screen)
-	bullets = Group()
-	aliens = Group()
+    bg_color = (230, 230, 230)
 
-	gf.create_fleet(ai_settings,screen,ship,aliens)
+    # alien = Alien(ai_settings,screen)
 
-	bg_color = (230, 230, 230)
+    while True:
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+        # bullets.update()
 
-	# alien = Alien(ai_settings,screen)
+        # for bullet in bullets.copy():
+        # 	if bullet.rect.bottom <= 0:
+        # 		bullets.remove(bullet)
+        # if len(bullets) > 0:
+        # 	print("bullets counts = ",len(bullets))
 
-	while True:
-		gf.check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets)
-		if stats.game_active:
-		    ship.update()
-		    gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-		    gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-		# bullets.update()
-
-		# for bullet in bullets.copy():
-		# 	if bullet.rect.bottom <= 0:
-		# 		bullets.remove(bullet)
-		# if len(bullets) > 0:
-		# 	print("bullets counts = ",len(bullets))
-
-		gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
-		# for event in pygame.event.get():
-		# 	if event.type == pygame.QUIT:
-		# 		sys.exit()
-			# else:
-				# bg_color = (random.random()*100,random.random()*200,random.random()*150)
-		# bg_color = (255, 0, 0)
-		# screen.fill(bg_color)
-		# screen.fill(ai_settings.bg_color)
-		# ship.blitme()
-		# pygame.display.flip()
-		# time.sleep(2)
-
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+    # for event in pygame.event.get():
+    # 	if event.type == pygame.QUIT:
+    # 		sys.exit()
+    # else:
+    # bg_color = (random.random()*100,random.random()*200,random.random()*150)
+    # bg_color = (255, 0, 0)
+    # screen.fill(bg_color)
+    # screen.fill(ai_settings.bg_color)
+    # ship.blitme()
+    # pygame.display.flip()
+    # time.sleep(2)
 
 
 run_game()
