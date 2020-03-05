412e0d7ada581957a21b2e47ce3cda6b19bef37a
hero <hero@hero.com>
2020-03-05 20:45:39 +0800

    reformat

--- ship.py
+++ ship.py
@@ -1,44 +1,37 @@
 import pygame
 
+
 class Ship():
 
-	def __init__(self,ai_settings,screen):
+    def __init__(self, ai_settings, screen):
 
-		self.screen = screen
-		self.ai_settings = ai_settings
-		self.screen_rect = screen.get_rect()
-		# print(type(self.screen_rect))
+        self.screen = screen
+        self.ai_settings = ai_settings
+        self.screen_rect = screen.get_rect()
+        # print(type(self.screen_rect))
 
-		self.image = pygame.image.load('images/ship.bmp')
-		
-		self.rect = self.image.get_rect()
-		self.rect.centerx  = self.screen_rect.centerx
-		self.rect.bottom = self.screen_rect.bottom
+        self.image = pygame.image.load('images/ship.bmp')
 
-		self.center = float(self.rect.centerx)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
-		self.moving_right = False
-		self.moving_left = False
+        self.center = float(self.rect.centerx)
 
+        self.moving_right = False
+        self.moving_left = False
 
-	def blitme(self):
-		self.screen.blit(self.image,self.rect)
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
 
-	def update(self):
-		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.center += self.ai_settings.ship_speed_factor
-			# self.rect.centerx +=1
-		if self.moving_left and self.rect.left > 0:
-			self.center -= self.ai_settings.ship_speed_factor
-			# self.rect.centerx -=1
-		self.rect.centerx = self.center
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        # self.rect.centerx +=1
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        # self.rect.centerx -=1
+        self.rect.centerx = self.center
 
-	def center_ship(self):
-		self.center = self.screen_rect.centerx
-
-
-	
-
-
-
-
+    def center_ship(self):
+        self.center = self.screen_rect.centerx
