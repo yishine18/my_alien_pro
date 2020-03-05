1faa4f57c62dbfe19c53a3d6fb41fddfb38028bc
hero <hero@hero.com>
2020-03-05 20:39:57 +0800

    reformate.

--- bullet.py
+++ bullet.py
@@ -1,26 +1,25 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Bullet(Sprite):
-	
-	def __init__(self,ai_settings,screen,ship):
-		super().__init__()
-		self.screen = screen
 
-		self.rect = pygame.Rect(0,0,ai_settings.bullet_width,ai_settings.bullet_height)
-		self.rect.centerx = ship.rect.centerx
-		self.rect.top = ship.rect.top
+    def __init__(self, ai_settings, screen, ship):
+        super().__init__()
+        self.screen = screen
 
-		self.y = float(self.rect.y)
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
 
-		self.color = ai_settings.bullet_color
-		self.speed_factor = ai_settings.bullet_speed_factor
+        self.y = float(self.rect.y)
 
-	def update(self):
-		self.y -= self.speed_factor
-		self.rect.y = self.y
+        self.color = ai_settings.bullet_color
+        self.speed_factor = ai_settings.bullet_speed_factor
 
-	def draw_bullet(self):
-		pygame.draw.rect(self.screen,self.color,self.rect)
+    def update(self):
+        self.y -= self.speed_factor
+        self.rect.y = self.y
 
-	
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
