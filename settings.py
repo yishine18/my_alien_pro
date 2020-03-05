e96a8349fea4874395d74fcb174218c79aaed6fb
hero <hero@hero.com>
2020-03-05 20:45:33 +0800

    reformat

--- settings.py
+++ settings.py
@@ -1,47 +1,35 @@
 class Settings():
-	def __init__(self):
-		self.screen_width = 1200
-		self.screen_height = 800
-		self.bg_color = (230, 230, 230)
-		self.ship_speed_factor = 2.5
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 2.5
 
+        self.bullet_speed_factor = 6
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
 
-		self.bullet_speed_factor = 6
-		self.bullet_width = 3	
-		self.bullet_height = 15
-		self.bullet_color = 60,60,60
-		self.bullets_allowed = 3
-		
+        self.alien_speed_factor = 6
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+        # fleet_direction = -1  表示左移
 
-		self.alien_speed_factor = 6
-		self.fleet_drop_speed = 10
-		self.fleet_direction = 1 
-		#fleet_direction = -1  表示左移
+        self.ship_limit = 2
 
-		self.ship_limit = 2
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
 
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 6
+        self.alien_speed_factor = 6
 
-		self.speedup_scale = 1.1
-		self.initialize_dynamic_settings()
-	def initialize_dynamic_settings(self):
-			
-		self.ship_speed_factor = 1.5
-		self.bullet_speed_factor = 6
-		self.alien_speed_factor = 6
+        self.fleet_direction = 1
+        self.alien_points = 50
 
-		self.fleet_direction = 1
-
-	def increase_speed(self):
-			
-		self.ship_speed_factor *= self.speedup_scale
-		self.bullet_speed_factor *= self.speedup_scale
-		self.alien_speed_factor *= self.speedup_scale
-
-
-		
-
-		
-
-
-
-
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
