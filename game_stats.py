c4d6d5d60f8c98bc7a7596c7ccc91984eac5c9ab
hero <hero@126.com>
2020-03-09 17:30:38 +0800

    add the high score atribute.

--- game_stats.py
+++ game_stats.py
@@ -4,6 +4,8 @@ class GameStats():
         # self.game_active = True
         self.reset_stats()
         self.game_active = False
+        self.high_score = 0
+
 
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_limit
