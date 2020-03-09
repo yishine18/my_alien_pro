5aae011360e496a818f10cd494858252ab87c0ad
hero <hero@126.com>
2020-03-09 17:31:16 +0800

    show high score .

--- scoreboard.py
+++ scoreboard.py
@@ -12,6 +12,18 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 48)
 
         self.prep_score()
+        self.pre_high_score()
+
+    def pre_high_score(self):
+        high_score = int(round(self.stats.high_score,-1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str,True,self.text_color,self.ai_settings.bg_color)
+
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+
 
     def prep_score(self):
         #score_str = str(self.stats.score)
@@ -26,3 +38,4 @@ class Scoreboard():
     def show_score(self):
         # print("this is from scoreboard!")
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image,self.high_score_rect)
