4e1050bb9d8d401f30efcbc6f8d2e047eafc519b
hero <hero@126.com>
2020-03-10 16:29:03 +0800

    add prep_score function .

--- scoreboard.py
+++ scoreboard.py
@@ -12,9 +12,10 @@ class Scoreboard():
         self.font = pygame.font.SysFont(None, 48)
 
         self.prep_score()
-        self.pre_high_score()
+        self.prep_high_score()
+        self.prep_level()
 
-    def pre_high_score(self):
+    def prep_high_score(self):
         high_score = int(round(self.stats.high_score,-1))
         high_score_str = "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str,True,self.text_color,self.ai_settings.bg_color)
@@ -39,3 +40,13 @@ class Scoreboard():
         # print("this is from scoreboard!")
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image,self.high_score_rect)
+        self.screen.blit(self.level_image,self.level_rect)
+
+
+    def prep_level(self):
+        self.level_image = self.font.render(str(self.stats.level),True,self.text_color,self.ai_settings.bg_color)
+
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
