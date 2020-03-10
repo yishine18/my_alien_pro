a749f6f2ece423cf3694ea12c302555337c8a2c5
hero <hero@126.com>
2020-03-10 16:30:00 +0800

    level plus ,prep_level.

--- game_functions.py
+++ game_functions.py
@@ -26,7 +26,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+def check_events(ai_settings, screen, stats,sb, play_button, ship, aliens, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -45,7 +45,7 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
         # 	ship.moving_left =False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats,sb,play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
 
 def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
@@ -162,6 +162,10 @@ def check_bullet_alien_collisions(ai_settings, screen, stats,sb,ship, aliens, bu
         bullets.empty()
 
         ai_settings.increase_speed()
+
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen, ship, aliens)
 
 
@@ -188,7 +192,7 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
             break
 
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats,sb, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     # if play_button.rect.collidepoint(mouse_x,mouse_y):
     if button_clicked and not stats.game_active:
@@ -197,6 +201,12 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         pygame.mouse.set_visible(False)
         stats.reset_stats()
         stats.game_active = True
+
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
+
+
 
         aliens.empty()
         bullets.empty()
@@ -207,6 +217,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
 def check_high_score(stats,sb):
     if stats.score > stats.high_score:
         stats.high_score = stats.score
-        sb.pre_high_score()
+        sb.prep_high_score()
+
 
 
